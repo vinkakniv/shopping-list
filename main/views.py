2ff935dba8b44ceaf49a327cf65315e3bfd2d150
vinkakniv <vinka.aeris@gmail.com>
2023-09-27 14:09:53 +0700

    tutorial 3 done

--- main/views.py
+++ main/views.py
@@ -18,7 +18,8 @@ def show_main(request):
     products = Product.objects.filter(user=request.user)
     context = {
         'name': request.user.username,
-        'class': 'PBP A', # Kelas PBP kamu
+        'class': 'PBP-A',
+        'id' : '2206820200',
         'products': products,
         'last_login': request.COOKIES['last_login'],
     }
@@ -86,3 +87,26 @@ def logout_user(request):
     response = HttpResponseRedirect(reverse('main:login'))
     response.delete_cookie('last_login')
     return response
+
+def edit_product(request, id):
+    # Get product berdasarkan ID
+    product = Product.objects.get(pk = id)
+
+    # Set product sebagai instance dari form
+    form = ProductForm(request.POST or None, instance=product)
+
+    if form.is_valid() and request.method == "POST":
+        # Simpan form dan kembali ke halaman awal
+        form.save()
+        return HttpResponseRedirect(reverse('main:show_main'))
+
+    context = {'form': form}
+    return render(request, "edit_product.html", context)
+
+def delete_product(request, id):
+    # Get data berdasarkan ID
+    product = Product.objects.get(pk = id)
+    # Hapus data
+    product.delete()
+    # Kembali ke halaman awal
+    return HttpResponseRedirect(reverse('main:show_main'))
